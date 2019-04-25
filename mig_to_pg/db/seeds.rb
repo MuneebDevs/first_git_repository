40f107e08554e3b0ff65a3b8edc25aa7f8bf7383
Muneeb-ur-Rehman <muneebjava@yahoo.com>
2019-04-25 13:00:44 +0000

    Changes from cloud9

--- mig_to_pg/db/seeds.rb
+++ mig_to_pg/db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# just add a new comment from cloud
 writer = Writer.create(name:'ali')
 
 before_time = Time.now
